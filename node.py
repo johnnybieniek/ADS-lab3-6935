9f586e3e07578731a22bc764ef471093e70379a6
johnnybieniek <jjbieniek@gmail.com>
2024-12-04 10:23:20 -0500

    final pre-cloud

--- node.py
+++ node.py
@@ -16,6 +16,7 @@ NODES = {
     'node2': {'ip': '10.128.0.5', 'port': 5002},
     'node3': {'ip': '10.128.0.6', 'port': 5003},
 }
+# For local testing
 #NODES = {
 #    'node1': {'ip': 'localhost', 'port': 5001},
 #    'node2': {'ip': 'localhost', 'port': 5002},
